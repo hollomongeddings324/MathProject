100f381a1a46cab86889c885a06b41e499d9ac0c
hollomongeddings324 <hollomongeddings324@gmail.com>
2025-03-17 19:11:33 +0000

    Add 12.ts

---
+++ src/12.ts
@@ -0,0 +1,8 @@
+let num = Math.floor(Math.random() * 10);
+if (num < 5) {
+    console.log("The number is less than 5");
+} else if (num > 5) {
+    console.log("The number is greater than 5");
+} else {
+    console.log("The number is equal to 5");
+}
