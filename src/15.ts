805230cea6a95b268b760fe71913ff16e50a89a6
hollomongeddings324 <hollomongeddings324@gmail.com>
2025-03-20 04:33:09 +0000

    Add 15.ts

---
+++ src/15.ts
@@ -0,0 +1,14 @@
+
+MathProject
+└── src
+    ├── main
+    │   └── java
+    │       └── com
+    │           └── mathproject
+    │               ├── MathFunctions.java
+    │               └── MathProject.java
+    └── test
+        └── java
+            └── com
+                └── mathproject
+                    └── MathProjectTest.java
