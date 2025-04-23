05667cc3f267775af9cfc3c5fc3fe565adb00e6c
hollomongeddings324 <hollomongeddings324@gmail.com>
2025-04-23 10:48:49 +0000

    Add 31.ts

---
+++ src/31.ts
@@ -0,0 +1,8 @@
+import { sqrt } from 'mathjs';
+
+function calculateSquareRoot(num: number): number {
+  return sqrt(num);
+}
+
+// Example usage
+console.log(calculateSquareRoot(16)); // Output: 4.0
